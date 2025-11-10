b762bedf2d05340ffb68f1026ec3a8a11e1bc3a5
trinhkhuyen <trinhkhuyen@gmail.com>
2025-11-11 03:16:02 +0700

    update code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx1234567890abcdef/exec"; // thay URL của bạn
+const API_URL = "https://script.google.com/macros/s/AKfycbz5UKQFVrxaFfCMRcqQ_78itWVm7q3HirEV4vWxgo3jO_37cKT9A6tgrAdqsKCOCKsnMQ/exec"; // thay URL của bạn
 
 const form = document.getElementById("surveyForm");
 const status = document.getElementById("status");
