5548d1adb86192d876661e3bfd731e719a5595fd
trinhkhuyen <trinhkhuyen@gmail.com>
2025-11-12 00:31:54 +0700

    Cach 2 cu

--- app_notOK.js
+++ app_notOK.js
@@ -1,7 +1,6 @@
 const form = document.getElementById("surveyForm");
 const status = document.getElementById("status");
 
-// API URL: link Google Apps Script đã triển khai
 const API_URL = "https://script.google.com/macros/s/AKfycbz5UKQFVrxaFfCMRcqQ_78itWVm7q3HirEV4vWxgo3jO_37cKT9A6tgrAdqsKCOCKsnMQ/exec";
 
 form.addEventListener("submit", async (e) => {
@@ -11,16 +10,21 @@ form.addEventListener("submit", async (e) => {
   const data = Object.fromEntries(new FormData(form).entries());
 
   try {
-    await fetch(API_URL, {
+    const res = await fetch(API_URL, {
       method: "POST",
-      mode: "no-cors", // tránh bị CORS chặn
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
 
-    status.textContent = "Đã gửi phản hồi.";
-    status.className = "status ok";
-    form.reset();
+    const json = await res.json();
+    if (json.ok) {
+      status.textContent = "Đã gửi phản hồi.";
+      status.className = "status ok";
+      form.reset();
+    } else {
+      status.textContent = "Lỗi xử lý ở server.";
+      status.className = "status err";
+    }
   } catch (err) {
     console.error(err);
     status.textContent = "Lỗi mạng hoặc API.";
